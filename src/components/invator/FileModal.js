799b22abb050bf62ca1f8212995860d127b81cf1
super-dev008 <super008008@outlook.com>
2022-06-02 14:20:42 +0300

    file modal

--- src/components/invator/FileModal.js
+++ src/components/invator/FileModal.js
@@ -1,10 +1,26 @@
 import React, { useState } from "react";
 import { Button, Container, Modal } from "react-bootstrap";
 
-const FileModal = () => {
+const FileModal = (props) => {
   const [show, setShow] = useState(true);
 
   const handleClose = () => setShow(false);
+
+  // Create a reference to the hidden file input element
+  const hiddenFileInput = React.useRef(null);
+
+  // Programatically click the hidden file input element
+  // when the Button component is clicked
+  const handleClick = (event) => {
+    hiddenFileInput.current.click();
+  };
+  // Call a function (passed as a prop from the parent component)
+  // to handle the user-selected file
+  const handleChange = (event) => {
+    const fileUploaded = event.target.files[0];
+    console.log(event.target.files);
+    props.handleFile(fileUploaded);
+  };
 
   return (
     <>
@@ -17,7 +33,7 @@ const FileModal = () => {
       >
         <Modal.Body className="text-center pt-0">
           <Container className="mt-5">
-            <span className="bg-primary px-3 pt-3 pb-2 rounded-circle">
+            <span className="bg-primary folder-padding rounded-circle">
               <i className="far fa-folder-open text-white text-center fs-4"></i>
             </span>
             <h5 className="mt-4">Let's get started.</h5>
@@ -32,10 +48,10 @@ const FileModal = () => {
             <div className="mt-3">
               <Button
                 className="bg-white width-100 rounded-3 p-5 border-dash"
-                type="file"
+                onClick={handleClick}
               >
-                <span className="px-3 pt-3 pb-2 rounded-circle bg-blues">
-                  <i class="fas fa-upload text-primary"></i>
+                <span className="upload-padding rounded-circle bg-blues">
+                  <i className="fas fa-upload text-primary"></i>
                 </span>
                 <p className="text-secondary fw-bold mt-3 mb-1">
                   Click to select a folder
@@ -44,6 +60,15 @@ const FileModal = () => {
                   Selected folder should contain a folder for each trait with a
                   file for each trait variation
                 </small>
+                <input
+                  type="file"
+                  id="hidden-upload"
+                  ref={hiddenFileInput}
+                  onChange={handleChange}
+                  style={{ display: "none" }}
+                  webkitdirectory="true"
+                  directory="true"
+                />
               </Button>
             </div>
           </Container>
